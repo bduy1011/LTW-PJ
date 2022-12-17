502f06c423622cd0d703e5a5d28f01cf494b8dbd
Huynh Tien Phat <2409huynhphat@gmail.com>
2022-12-18 00:28:50 +0700

    create function crawl data list song

--- ZingMp3/ZingMp3/obj/Debug/net6.0-windows/App.g.cs
+++ ZingMp3/ZingMp3/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace ZingMp3 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ZingMp3 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public static void Main() {
             ZingMp3.App app = new ZingMp3.App();
             app.InitializeComponent();
