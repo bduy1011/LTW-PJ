972e85d1c2d2d87b350f391994eae2632df60434
Phat723 <21522449@gm.uit.edu.vn>
2022-12-21 01:19:24 +0700

    add workSpace and workTab

---
+++ Basic_Photo_Editor/Basic_Photo_Editor/workSpace.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Photo_Editor
+{
+    public partial class workSpace : UserControl
+    {
+        //public History History { get; set; }
+        //public DrawSpace DrawSpace { get; set; }
+        public LayerContainer LayerContainer { get; set; }
+        public Rectangle Rect { get; set; }
+        public Size BmpSize { get; set; }
+        public System.Drawing.Imaging.PixelFormat BmpPixelFormat { get; set; }
+
+        string filename;
+        public string FileName
+        {
+            get => filename;
+            set => filename = value;
+        }
+
+        string filepath;
+        public string FilePath
+        {
+            get => filepath;
+            set
+            {
+                filepath = value;
+                if (value != "")
+                {
+                    string name = "";
+                    foreach (char c in filepath)
+                    {
+                        if (c == '\\') name = "";
+                        else name += c;
+                    }
+                    filename = name;
+                }
+            }
+        }
+        public bool Working { get; set; }
+        public bool Saved { get; set; }
+        public bool Stored { get; set; }
+
+        #region Can sửa contructor
+        public workSpace(LayerContainer lc)
+        {
+            InitializeComponent();
+            Dock = DockStyle.Fill;
+            AutoScroll = true;
+            //DrawSpace = ds;
+            LayerContainer = lc;
+            //History = h;
+            Working = false;
+            Saved = true;
+            Stored = false;
+            filepath = filename = "";
+            //this.Controls.Add(DrawSpace);
+        }
+        #endregion
+    }
+}
---
+++ Basic_Photo_Editor/Basic_Photo_Editor/workTabControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Photo_Editor
+{
+    public partial class workTabControl : Component
+    {
+        public workTabControl()
+        {
+            InitializeComponent();
+        }
+
+        public workTabControl(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
