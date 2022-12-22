e75626b00e0ebdf88194fda866e174bb61483ee4
Huynh Tien Phat <2409huynhphat@gmail.com>
2022-12-22 13:22:27 +0700

    Add UC DrawSpace to control draw in FormMain

---
+++ Basic_Photo_Editor/Basic_Photo_Editor/DrawSpace.Designer.cs
@@ -0,0 +1,97 @@
+﻿using System.Diagnostics;
+
+namespace Basic_Photo_Editor
+{
+    partial class DrawSpace
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            if (processing != null) processing.Dispose();
+            if (g != null) g.Dispose();
+            gF.Dispose();
+            gFinal.Dispose();
+            gTop.Dispose();
+            gProcess.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.finalBox = new System.Windows.Forms.PictureBox();
+            this.processBox = new System.Windows.Forms.PictureBox();
+            this.topBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.finalBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.processBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // finalBox
+            // 
+            this.finalBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.finalBox.Location = new System.Drawing.Point(0, 0);
+            this.finalBox.Name = "finalBox";
+            this.finalBox.Size = new System.Drawing.Size(150, 150);
+            this.finalBox.TabIndex = 0;
+            this.finalBox.TabStop = false;
+            this.finalBox.Controls.Add(this.processBox);
+            // 
+            // processBox
+            // 
+            this.processBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.processBox.Location = new System.Drawing.Point(0, 0);
+            this.processBox.Name = "processBox";
+            this.processBox.Size = new System.Drawing.Size(150, 150);
+            this.processBox.TabIndex = 1;
+            this.processBox.TabStop = false;
+            this.processBox.Controls.Add(this.topBox);
+            // 
+            // topBox
+            // 
+            this.topBox.BackColor = System.Drawing.Color.Transparent;
+            this.topBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.topBox.Location = new System.Drawing.Point(0, 0);
+            this.topBox.Name = "topBox";
+            this.topBox.Size = new System.Drawing.Size(150, 150);
+            this.topBox.TabIndex = 3;
+            this.topBox.TabStop = false;
+            // 
+            // DrawSpace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.finalBox);
+            this.Name = "DrawSpace";
+            ((System.ComponentModel.ISupportInitialize)(this.finalBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.processBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox finalBox;
+        private System.Windows.Forms.PictureBox processBox;
+        private System.Windows.Forms.PictureBox topBox;
+    }
+}
---
+++ Basic_Photo_Editor/Basic_Photo_Editor/DrawSpace.cs
@@ -0,0 +1,206 @@
+﻿using Basic_Photo_Editor.Paint_Tools;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Photo_Editor
+{
+    public partial class DrawSpace : UserControl
+    {
+        private Bitmap processing;
+        Graphics gFinal;
+        Graphics gF;
+        Graphics gProcess;
+        Graphics gTop;
+        Graphics g;
+        Size originalSize;
+        float zoom;
+        public float Zoom { get => zoom; }
+        public Matrix ScaleMatrix { get; set; }
+        public Tools Tools { get; set; }
+
+        public DrawSpace()
+        {
+            InitializeComponent();
+            ScaleMatrix = new Matrix();
+            zoom = 1f;
+        }
+
+        public void Init()
+        {
+            processing = new Bitmap(this.Size.Width, this.Size.Height);
+            Final = new Bitmap(this.Size.Width, this.Size.Height);
+            g = Graphics.FromImage(processing);
+            gF = Graphics.FromImage(Final);
+            InitGraphic();
+
+            originalSize = this.Size;
+        }
+
+        public void SetCenter()
+        {
+            WorkSpace ws = (WorkSpace)this.Parent;
+
+            float z = 1f;
+            float a = (float)originalSize.Width / originalSize.Height;
+            float b = (float)ws.Width / ws.Height;
+
+            if (a > b) z = (float)(ws.Width - 40) / originalSize.Width;
+            else z = (float)(ws.Height - 40) / originalSize.Height;
+
+            Scaling(z);
+
+            this.Left = ws.Width / 2 - this.Width / 2;
+            this.Top = ws.Height / 2 - this.Height / 2;
+        }
+
+        void InitGraphic()
+        {
+            finalBox.Image = new Bitmap(this.Size.Width, this.Size.Height);
+            gFinal = Graphics.FromImage(finalBox.Image);
+            processBox.Image = new Bitmap(this.Size.Width, this.Size.Height);
+            gProcess = Graphics.FromImage(processBox.Image);
+            topBox.Image = new Bitmap(this.Size.Width, this.Size.Height);
+            gTop = Graphics.FromImage(topBox.Image);
+        }
+
+        public void Scaling(float scale)
+        {
+            zoom = scale;
+            Size newSize = new Size((int)(originalSize.Width * zoom), (int)(originalSize.Height * zoom));
+            this.Size = newSize;
+            InitGraphic();
+            ScaleMatrix.Reset();
+            ScaleMatrix.Scale(scale, scale);
+            Invalidate();
+
+        }
+
+        PointF ScaledPoint(PointF p)
+        {
+            return new PointF(p.X / zoom, p.Y / zoom);
+        }
+
+        public PictureBox Event
+        {
+            get
+            {
+                return topBox;
+            }
+        }
+
+        public Bitmap Final { get; set; }
+
+        public Graphics Final_Graphics
+        {
+            get
+            {
+                return gF;
+            }
+        }
+
+        public void FinalDisplay()
+        {
+            gFinal.Clear(Color.Transparent);
+            gFinal.MultiplyTransform(ScaleMatrix);
+            gFinal.DrawImageUnscaled(Final, 0, 0);
+            gFinal.ResetTransform();
+        }
+
+        public Bitmap CurrentLayerImg { get; set; }
+
+        public bool CurrentVisible { get; set; }
+        public Image ProcessBoxImage
+        {
+            set
+            {
+                if (!Tools.Select.Selected)
+                    g.DrawImageUnscaled(value, 0, 0);
+                else
+                    g.DrawImageUnscaled(value, Tools.Select.FixedRect.X, Tools.Select.FixedRect.Y,
+                        Tools.Select.FixedRect.Width, Tools.Select.FixedRect.Height);
+
+                value.Dispose();
+            }
+            get
+            {
+                return processing;
+            }
+        }
+
+        public void ClearProcess()
+        {
+            if (processing == null) return;
+            g.Clear(Color.Transparent);
+        }
+        public void ClearTop()
+        {
+            gTop.Clear(Color.Transparent);
+            topBox.Invalidate();
+        }
+
+        public void BGGenerator(Color c)
+        {
+            if (c == Color.Transparent)
+            {
+                int m = (int)Math.Ceiling((double)Size.Width / 512);
+                int n = (int)Math.Ceiling((double)Size.Height / 512);
+                Bitmap bg = new Bitmap(512 * m, 512 * n);
+                using (Graphics g = Graphics.FromImage(bg))
+                {
+                    for (int i = 0; i < m; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            g.DrawImage(Properties.Resources.TransparencyBG, i * 512, j * 512);
+                        }
+                    }
+                    BackgroundImage = bg;
+                }
+            }
+            else
+            {
+                Bitmap bg = new Bitmap(Size.Width, Size.Height);
+                using (Graphics g = Graphics.FromImage(bg))
+                {
+                    using (SolidBrush brush = new SolidBrush(c))
+                        g.FillRectangle(brush, 0, 0, Size.Width, Size.Height);
+                    BackgroundImage = bg;
+                }
+            }
+        }
+
+        RectangleF displayRect;
+        PointF p1;
+        PointF p2;
+        void ProcessDisplay()
+        {
+            PointF p = new PointF(Math.Min(p1.X - (float)Tools.Size / 2 - 1, p2.X - (float)Tools.Size / 2 - 1),
+                                Math.Min(p1.Y - (float)Tools.Size / 2 - 1, p2.Y - (float)Tools.Size / 2 - 1));
+
+            displayRect = new RectangleF(p.X, p.Y,
+                                        Math.Abs(p2.X - p1.X) + Tools.Size + 2,
+                                        Math.Abs(p2.Y - p1.Y) + Tools.Size + 2);
+
+            gProcess.MultiplyTransform(ScaleMatrix);
+            gProcess.DrawImage(processing, p.X, p.Y, displayRect, GraphicsUnit.Pixel);
+            gProcess.ResetTransform();
+        }
+
+        public void SelectRectDisplay()
+        {
+            gTop.Clear(Color.Transparent);
+            gTop.MultiplyTransform(ScaleMatrix);
+            Tools.Select.DrawRect(gTop);
+            gTop.ResetTransform();
+            topBox.Invalidate();
+        }
+    }
+}
