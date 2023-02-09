8b9c6b0b82127d551790012c29f144b560738a55
Phat723 <21522449@gm.uit.edu.vn>
2023-02-09 13:18:01 +0700

    Remake brightness and contrast event

--- Basic_Photo_Editor/Basic_Photo_Editor/Function_Forms/Brightness_Contrast.Designer.cs
+++ Basic_Photo_Editor/Basic_Photo_Editor/Function_Forms/Brightness_Contrast.Designer.cs
@@ -47,44 +47,46 @@
             // 
             this.brightnessTrack.AutoSize = false;
             this.brightnessTrack.LargeChange = 1;
-            this.brightnessTrack.Location = new System.Drawing.Point(105, 31);
-            this.brightnessTrack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.brightnessTrack.Location = new System.Drawing.Point(93, 25);
+            this.brightnessTrack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.brightnessTrack.Maximum = 100;
             this.brightnessTrack.Minimum = -100;
             this.brightnessTrack.Name = "brightnessTrack";
-            this.brightnessTrack.Size = new System.Drawing.Size(450, 45);
+            this.brightnessTrack.Size = new System.Drawing.Size(400, 36);
             this.brightnessTrack.TabIndex = 1;
             this.brightnessTrack.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.brightnessTrack.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
             // 
             // contrastTrack
             // 
             this.contrastTrack.LargeChange = 10;
-            this.contrastTrack.Location = new System.Drawing.Point(105, 86);
-            this.contrastTrack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.contrastTrack.Location = new System.Drawing.Point(93, 69);
+            this.contrastTrack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.contrastTrack.Maximum = 100;
             this.contrastTrack.Minimum = -100;
             this.contrastTrack.Name = "contrastTrack";
-            this.contrastTrack.Size = new System.Drawing.Size(450, 69);
+            this.contrastTrack.Size = new System.Drawing.Size(400, 56);
             this.contrastTrack.TabIndex = 1;
             this.contrastTrack.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.contrastTrack.Scroll += new System.EventHandler(this.TrackBar2_Scroll);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 32);
+            this.label1.Location = new System.Drawing.Point(9, 26);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.Size = new System.Drawing.Size(70, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Brightness";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 91);
+            this.label2.Location = new System.Drawing.Point(9, 73);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.Size = new System.Drawing.Size(56, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "Contrast";
             // 
@@ -94,10 +96,10 @@
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(350, 158);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(311, 126);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 32);
+            this.button1.Size = new System.Drawing.Size(100, 26);
             this.button1.TabIndex = 3;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = false;
@@ -109,10 +111,10 @@
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.button2.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(471, 158);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Location = new System.Drawing.Point(419, 126);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 32);
+            this.button2.Size = new System.Drawing.Size(100, 26);
             this.button2.TabIndex = 3;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = false;
@@ -121,20 +123,20 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(564, 32);
+            this.label3.Location = new System.Drawing.Point(501, 26);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.Size = new System.Drawing.Size(14, 16);
             this.label3.TabIndex = 4;
             this.label3.Text = "0";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(564, 91);
+            this.label4.Location = new System.Drawing.Point(501, 73);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.Size = new System.Drawing.Size(14, 16);
             this.label4.TabIndex = 4;
             this.label4.Text = "0";
             // 
@@ -143,10 +145,10 @@
             this.button3.BackColor = System.Drawing.Color.Gray;
             this.button3.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(15, 158);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Location = new System.Drawing.Point(13, 126);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(112, 32);
+            this.button3.Size = new System.Drawing.Size(100, 26);
             this.button3.TabIndex = 3;
             this.button3.Text = "Reset";
             this.button3.UseVisualStyleBackColor = false;
@@ -154,10 +156,10 @@
             // 
             // Brightness_Contrast
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(640, 274);
+            this.ClientSize = new System.Drawing.Size(569, 219);
             this.ControlBox = false;
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -170,7 +172,7 @@
             this.Controls.Add(this.brightnessTrack);
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Brightness_Contrast";
